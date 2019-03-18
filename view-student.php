9edb3bb39e8570681470069c2d5acc3023ac7410
Minhaj-Khan <minhaj.mehdi.khan@gmail.com>
2019-03-19 03:06:02 +0600

    SQL start and form create, add and update with creating a database

---
+++ view-student.php
@@ -0,0 +1,34 @@
+<?php
+require_once "vendor/autoload.php";
+use App\classes\Student;
+$message=" ";
+
+$queryResult=Student::getAllStudentInfo();
+
+//while ($student=mysqli_fetch_assoc($queryResult))
+//{
+//    echo "<pre>";
+//    print_r($student);
+//}
+
+?>
+</hr>
+<a href="add-student.php">Add Student</a>
+<a href="view-student.php">View Student</a>
+<h1 style="color: green;"><?php echo $message;?></h1>
+<table border="1">
+    <tr>
+        <th align="center">ID</th>
+        <th>Student Name</th>
+        <th>Student Email</th>
+        <th>Student Mobile</th>
+    </tr>
+    <?php while ($student=mysqli_fetch_assoc($queryResult)){?>
+    <tr>
+        <td><?php echo $student['id'];?></td>
+        <td><?php echo $student['name'];?></td>
+        <td><?php echo $student['email'];?></td>
+        <td><?php echo $student['mobile'];?></td>
+    </tr>
+    <?php }?>
+</table>
